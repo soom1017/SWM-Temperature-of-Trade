26ac3a9c92651a32414959973b1c2f601af35d86
leesoomin <min001017@hanyang.ac.kr>
2022-10-09 18:28:55 +0900

    fix: firebase error fixed, session to token

--- app/api/users.py
+++ app/api/users.py
@@ -1,3 +1,4 @@
+from logging import Logger
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -9,48 +10,44 @@ from app.db.database import get_db
 
 import requests
 import firebase_admin
+from firebase_admin import auth, credentials
 
 FIREBASE_CERT_PATH = oauthsettings.FIREBASE_CERT_PATH
-
-KAKAO_REST_API_KEY = oauthsettings.KAKAO_REST_API_KEY
-KAKAO_REDIRECT_URI = oauthsettings.KAKAO_REDIRECT_URI
 KAKAO_URI = "https://kapi.kakao.com/v1/user/access_token_info"
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(FIREBASE_CERT_PATH)
+    app = firebase_admin.initialize_app(cred)
 
 users = APIRouter()
 
 # Register / Login
 @users.post('/auth/kakao')
-async def kakao_auth_request(authdata: AuthData, db: Session = Depends(get_db)):
-    headers = {'Authorization': f'Bearer {authdata.access_token}'}
-    res = requests.get(KAKAO_URI, headers=headers)
-    
-    if res.status_code == 200:
-        # authorized user, but not signed up
-        user = db.query(models.User).filter(models.User.id == authdata.uid).first()
-        if not user:
-            new_user = models.User(id=authdata.uid, access_token=authdata.access_token)
-            db.add(new_user)
-            db.commit()
-        
-        # finish sign-in    
-        cred = firebase_admin.credentials.Certificate(FIREBASE_CERT_PATH)
-        app = firebase_admin.initialize_app(cred, name='auth')
+async def kakao_auth_request(uid: str, db: Session = Depends(get_db)):
 
-        firebase_admin.auth.get_user(authdata.uid, app)
-        custom_token = firebase_admin.create_custom_token(authdata.uid, app=app)
-        return custom_token.decode('utf-8')
+    # authorized user, but not signed up
+    user = db.query(models.User).filter(models.User.id == uid).first()
+    if not user:
+        new_user = models.User(id=uid)
+        db.add(new_user)
+        db.commit()
+        # add to firebase
+        auth.create_user()
     
-    return HTTPException(status_code=401, detail="Token is not valid")
+    # finish sign-in    
+    auth.get_user(uid, app)
+    custom_token = auth.create_custom_token(uid, app=app)
+    return custom_token.decode('utf-8')
 
-@users.get('/{user_id}/bookmarks', response_model=List[schemas.News])
-async def get_user_bookmark_news(user_id: str, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-    if user == None:
-        raise HTTPException(404, detail="User not found")
-    
-    headers = {'Authorization': f'Bearer {user.access_token}'}
+@users.get('/{authData}/bookmarks', response_model=schemas.NewsOut)
+async def get_user_bookmark_news(authData: AuthData, db: Session = Depends(get_db)):
+    headers = {'Authorization': f'Bearer {authData.access_token}'}
     res = requests.get(KAKAO_URI, headers=headers)
     if res.status_code != 200:
         raise HTTPException(404, detail="Token is not valid")
     
-    return user.news
+    user = db.query(models.User).filter(models.User.id == authData.user_id).first()
+    if user == None:
+        raise HTTPException(404, detail="User not found")
+
+    return {"data": user.news}
--- app/db/models.py
+++ app/db/models.py
@@ -35,7 +35,6 @@ class User(Base):
     __tablename__ = "user"
     
     id = Column(String(50), primary_key=True)
-    access_token = Column(String(50))
     main_page_id = Column(Integer)
     
     news = relationship('News', secondary=user_bookmark_news, back_populates="user")
