4809365d05b625ad2156723080a06cfd85d344d8
leesoomin <min001017@hanyang.ac.kr>
2022-11-04 05:06:00 +0900

    feat: result in softmax type, low percentage to

--- news_process/utils/stockmatcher.py
+++ news_process/utils/stockmatcher.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from neo4j import GraphDatabase
+import numpy as np
   
 driver = GraphDatabase.driver("bolt://test4j:7687", auth=("neo4j", "test"))
 stocks = [
@@ -10,7 +11,16 @@ stocks = [
 #   "LG화학" ,"셀트리온","카카오","KB금융","기아","삼성전자우"
 ]
 possible_stocks = [
-    '삼성전자', 'LG에너지솔루션', '삼성바이오로직스', 'SK하이닉스', '삼성SDI', 'LG화학', '삼성전자우', '현대자동차', '네이버', '셀트리온', '기아', '카카오', '삼성물산', 'POSCO홀딩스', '현대모비스', 'KB금융', '신한지주', 'SK이노베이션', 'SK', '포스코케미칼', '삼성생명', 'LG전자', 'KT&G', '고려아연', 'LG', '하나금융지주', 'SK텔레콤', '한국전력', 'S-Oil', '현대중공업', '삼성에스디에스', 'KT', '삼성화재', 'HMM', '삼성전기', '한화솔루션', '크래프톤', '엔씨소프트', '우리금융지주', '대한항공', '두산에너빌리티', '카카오뱅크', 'LG생활건강', '기업은행', 'LG이노텍', '현대글로비스', 'CJ제일제당', 'SK바이오사이언스', 'F&F', '아모레퍼시픽', '포스코홀딩스'
+    '삼성전자', 'LG에너지솔루션', '삼성바이오로직스', 'SK하이닉스', '삼성SDI',
+    'LG화학', '삼성전자우', '현대자동차', '네이버', '셀트리온',
+    '기아', '카카오', '삼성물산', 'POSCO홀딩스', '현대모비스',
+    'KB금융', '신한지주', 'SK이노베이션', 'SK', '포스코케미칼',
+    '삼성생명', 'LG전자', 'KT&G', '고려아연', 'LG',
+    '하나금융지주', 'SK텔레콤', '한국전력', 'S-Oil', '현대중공업',
+    '삼성에스디에스', 'KT', '삼성화재', 'HMM', '삼성전기',
+    '한화솔루션', '크래프톤', '엔씨소프트', '우리금융지주', '대한항공',
+    '두산에너빌리티', '카카오뱅크', 'LG생활건강', '기업은행', 'LG이노텍',
+    '현대글로비스', 'CJ제일제당', 'SK바이오사이언스', 'F&F', '아모레퍼시픽', '포스코홀딩스'
 ]
 default_weight = 8
 
@@ -18,6 +28,36 @@ def run_query(input_query):
     with driver.session() as session: 
         results = session.run(input_query)
         return list(results)
+    
+def softmax(x):
+    x = np.exp(np.array(x))
+    sum_x = np.sum(x)
+    out = x / sum_x
+    return out
+
+def rearrange_prob(stock_prob: dict):
+    stock = list(stock_prob.keys())
+    prob_val = list(stock_prob.values())
+    prob_val = softmax(prob_val)
+    prob_val = [round(val, 4) * 100 for val in prob_val if round(val, 4)]
+    # 처음 세개합 < 70: 종합
+    if len(prob_val) > 3 and sum(prob_val[:3]) < 50:
+        return "종합", ""
+    # 맨 끝 25 내외 정도는 '기타'로 처리
+    etc, prev = 0, 0
+    for i, val in enumerate(sorted(prob_val)):
+        if etc + prev > 25 or etc + prev > val:
+            break
+        etc += prev
+        prev = val
+        
+    if etc:
+        prob_val = prob_val[:-i] + [etc]
+        stock = stock[:-i] + ["기타"]
+    # 더해서 100 나오는 거에 강박 있는 편
+    prob_val[-1] = round(prob_val[-1] + (100 - sum(prob_val)), 2)
+        
+    return stock[0], {stock[i]: prob_val[i] for i in range(len(prob_val)) if prob_val[i]}
     
 class StockMatcher:
     def __init__(self):
@@ -46,16 +86,16 @@ class StockMatcher:
             weight /= 2
         
     
-    def get_attention_stock(self, text: str, tfidf: dict, N: int = 3):        
+    def get_attention_stock(self, text: str, tfidf: dict, N: int = 5):        
         # try exact-matching
         exactly_matched = [st for st in possible_stocks if st in text]
         if exactly_matched:
-            if len(exactly_matched) > 3:
-                return "종합", None
+            if len(exactly_matched) > 5:
+                return "종합", ""
             
             prob = {st: text.count(st) for st in exactly_matched}
-            sorted_prob = sorted(prob.items(), key=lambda item: item[1])
-            return sorted_prob[0][0], dict(sorted_prob)
+            sorted_prob = sorted(prob.items(), key=lambda item: item[1], reverse=True)
+            return rearrange_prob(dict(sorted_prob))
         
         # no matched stocks, then try graph-matching
         self.prob = {stock : 0 for stock in possible_stocks}
@@ -65,6 +105,6 @@ class StockMatcher:
             
         sorted_prob = sorted(self.prob.items(), key=lambda item: item[1], reverse=True)[:N]
         if not sorted_prob[0][1]:
-            return "없음", ""
-        return sorted_prob[0][0], dict(sorted_prob)
+            return "종합", ""
+        return rearrange_prob(dict(sorted_prob))
 
